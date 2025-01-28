6449ef05f1a0f5911cef09a7751cea394422a8b1
Alex <basurillodesgastorito@gmail.com>
2025-01-28 21:24:52 +0100

    Fuck ton of updates.

---
+++ src/lib/contentful.ts
@@ -0,0 +1,21 @@
+import contentful, { type Entry, type EntryFieldTypes } from "contentful";
+
+export interface work {
+  contentTypeId: "blogTars";
+  fields: {
+    title: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    date: EntryFieldTypes.Date;
+    headerImg: EntryFieldTypes.AssetLink;
+    description: EntryFieldTypes.Text;
+    content: EntryFieldTypes.RichText;
+  };
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
